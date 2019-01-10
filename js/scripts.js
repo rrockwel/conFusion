9a7726a358b9daf22093fa12e9c1e8e8dfcf85ed
rrockwel <vtboarder69@gmail.com>
2019-01-10 18:46:28 -0500

    NPM Scripts

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    //Enable tool-tip on 'reserve table' button
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //Set carousel interval to 2 seconds
+    $('#mycarousel').carousel({interval: 2000});
+
+    //Set carousel button to cycle between 'play' and 'pause' button
+    $('#carousel-button').click(function() {
+        if($('#carousel-button').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carousel-button').children('span').removeClass('fa-pause');
+            $('#carousel-button').children('span').addClass('fa-play');
+        }
+        else if($('#carousel-button').children('span').hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carousel-button').children('span').removeClass('fa-play');
+            $('#carousel-button').children('span').addClass('fa-pause');
+        }
+    })
+
+    $('#loginButton').click(function(){
+        $('#loginModal').modal('show');
+    });
+
+    $('#reserveButton').click(function(){
+        $('#reserveTableModal').modal('show');
+
+    });
+});
